bdb09383f6dec8ce17625f69ee688978a0baf8ec
Prudhvinath Reddymalla <46321899+prudhvinathreddymalla@users.noreply.github.com>
2020-03-01 01:44:48 +0100

    Update classification.py

--- classification.py
+++ classification.py
@@ -9,9 +9,6 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-seed = np.random.randint(100)
-#83
-np.random.seed(seed)
 
 import warnings
 warnings.filterwarnings('ignore')
